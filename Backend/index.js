b83fb02878c4274281636b00a20269f8012e2542
Ganesh-Patel-U <ganesh.oficial158@gmail.com>
2024-10-10 12:19:43 +0530

    same site issue in login

--- Backend/index.js
+++ Backend/index.js
@@ -11,12 +11,22 @@ import CouponRouter from './Routes/CouponsRouter.js';
 import addressRoutes from './Routes/addressRoutes.js'
 import orderRouter from './Routes/orderRoutes.js';
 
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://apni-shop-eight.vercel.app' 
+];
+
 const corsOptions = {
-    origin: '*', // Your frontend origin
-    credentials: true, // This allows the server to accept cookies
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"], // Allowed methods
-    
-  };
+  origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+          callback(null, true);
+      } else {
+          callback(new Error('Not allowed by CORS'));
+      }
+  },
+  credentials: true, 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+};
 
 const app = express();
 app.use(cors(corsOptions));
