92bb64bf7fcfad38f36b45ff7c79deed71ad4f4a
Ganesh <ganesh.oficial158@gmail.com>
2024-12-21 19:04:16 +0530

    loading issue at home

--- Frontend/src/Contexts/UserContext.jsx
+++ Frontend/src/Contexts/UserContext.jsx
@@ -2,6 +2,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import {isUserLoggedIn} from '../Utils/api.js'
 import { Navigate, useNavigate } from 'react-router-dom';
+import { Puff } from 'react-loader-spinner';
 export const UserContext = createContext();
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null); 
@@ -22,7 +23,19 @@ export const UserProvider = ({ children }) => {
   }, []);
   console.log(isLoggedIn)
   if (loading) {
-    return null;
+    return (
+      <div className="flex justify-center items-center h-screen">
+      <Puff
+        height={80}
+        width={80}
+        radius={1}
+        color="#38b2ac"
+        ariaLabel="loading"
+        visible={true}
+      />
+      <h1>Render is taking time please wait Loading....</h1>
+    </div>
+    );
   }
   return (
     <UserContext.Provider value={{ user, setUser,isLoggedIn,setIsLoggedIn,loading }}>
